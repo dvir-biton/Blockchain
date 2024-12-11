a1337e11aff2569f090223921563abdef3bffc49
fylora <fylora189@gmail.com>
2024-12-11 19:14:57 +0200

    Updated BlockFactory hash

--- src/factory/BlockFactory.java
+++ src/factory/BlockFactory.java
@@ -6,10 +6,15 @@ import entities.Block;
 public class BlockFactory {
     public static Block createBlock(
         final String previousHash,
+        final long timestamp,
         final String data
     ) {
         return new Block(
-            DependencyInjection.getHashService().hash(data),
+            DependencyInjection.getHashService().hash(
+                previousHash +
+                timestamp +
+                data
+            ),
             previousHash,
             data
         );
