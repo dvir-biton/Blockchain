e5be01eadaa866901474367639c28e972434fc90
fylora <fylora189@gmail.com>
2024-12-11 19:05:48 +0200

    Added BlockFactory

--- src/factory/BlockFactory.java
+++ src/factory/BlockFactory.java
@@ -1,5 +1,17 @@
 package factory;
 
-public class BlockFactory {
+import di.DependencyInjection;
+import entities.Block;
 
+public class BlockFactory {
+    public static Block createBlock(
+        final String previousHash,
+        final String data
+    ) {
+        return new Block(
+            DependencyInjection.getHashService().hash(data),
+            previousHash,
+            data
+        );
+    }
 }
